bb2e0bcdd103f5a5eeff933169aac06e71d24fa2
Axente Andrei <axenteandrei@Axentes-MacBook-Air.local>
2022-10-19 20:56:29 +0300

    My first swift project!

---
+++ ThemePicker.swift
@@ -0,0 +1,26 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Axente Andrei on 29.06.2022.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
